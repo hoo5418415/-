b7a4ef8614f4f9b33274b0b1260853d9060d7ae9
hoo5418415 <56114801+hoo5418415@users.noreply.github.com>
2020-05-19 17:25:06 +0800

    upload exercise 1

---
+++ exercise1/src/main/java/selab/myapp/Hello.java
@@ -0,0 +1,13 @@
+package selab.myapp;
+
+public class Hello {
+
+  /**.
+  * Just Homework 
+  */
+  public static void main(String[] args) {
+  
+    System.out.println("Hello everyone,");
+    System.out.print("I am studying at Feng Chia University.");
+  }
+}
